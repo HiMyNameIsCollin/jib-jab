0b3963345a337e7e5a225c50ac814cd84d804857
Innuendo416 <collinstone2291@icloud.com>
2020-12-02 11:08:01 -0500

    Inbox start

--- client/src/pageTemplates/inboxPage/InboxPage.js
+++ client/src/pageTemplates/inboxPage/InboxPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import '_inboxPage.sass'
+import './_inboxPage.sass'
 
 const InboxPage = () => {
 	return(
 		<div className='inboxPage'>
-		
+			<div className='container'>
+				<h3> Inbox </h3>
+			</div>
 		</div> 
 	)
 }
